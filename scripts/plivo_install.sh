62198ac4883279fe34bb68b85e9770721950f71f
MayamaTakeshi <mayamatakeshi@gmail.com>
2014-02-01 09:05:46 +0900

    changing scripts/plivo_install.sh to use MayamaTakeshi/plivoframework

--- scripts/plivo_install.sh
+++ scripts/plivo_install.sh
@@ -5,8 +5,10 @@
 # Copyright (c) 2011 Plivo Team. See LICENSE for details.
 
 
-PLIVO_GIT_REPO=git://github.com/plivo/plivoframework.git
-PLIVO_SETUP_SCRIPT=https://raw.github.com/plivo/plivoframework/master/scripts/ez_setup.py
+#PLIVO_GIT_REPO=git://github.com/plivo/plivoframework.git
+PLIVO_GIT_REPO=https://github.com/MayamaTakeshi/plivoframework.git
+#PLIVO_SETUP_SCRIPT=https://raw.github.com/plivo/plivoframework/master/scripts/ez_setup.py
+PLIVO_SETUP_SCRIPT=https://raw.github.com/MayamaTakeshi/plivoframework/master/scripts/ez_setup.py
 
 
 BRANCH=$2
