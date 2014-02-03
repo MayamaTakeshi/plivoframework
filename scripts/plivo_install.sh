feb3164adba6cc245b2687e3292ff5aa51b64849
MayamaTakeshi <mayamatakeshi@gmail.com>
2014-02-03 12:23:19 +0900

    plivo_install.sh: set to use git url to fetch this repo as https is causing problems in HBANs

--- scripts/plivo_install.sh
+++ scripts/plivo_install.sh
@@ -6,7 +6,7 @@
 
 
 #PLIVO_GIT_REPO=git://github.com/plivo/plivoframework.git
-PLIVO_GIT_REPO=https://github.com/MayamaTakeshi/plivoframework.git
+PLIVO_GIT_REPO=git://github.com/MayamaTakeshi/plivoframework.git
 #PLIVO_SETUP_SCRIPT=https://raw.github.com/plivo/plivoframework/master/scripts/ez_setup.py
 PLIVO_SETUP_SCRIPT=https://raw.github.com/MayamaTakeshi/plivoframework/master/scripts/ez_setup.py
 
@@ -166,7 +166,7 @@ gpgcheck = 1
 
             # Install easy_install
             cd $DEPLOY/source
-            wget --no-check-certificate https://github.com/plivo/plivoframework/raw/master/scripts/ez_setup.py
+            wget --no-check-certificate https://github.com/MayamaTakeshi/plivoframework/raw/master/scripts/ez_setup.py
             $DEPLOY/bin/python ez_setup.py
 
             EASY_INSTALL=$(which easy_install)
