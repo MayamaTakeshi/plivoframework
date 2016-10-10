13abec464d01eccb2476b39d9d77c44e2d763408
MayamaTakeshi <mayamatakeshi@gmail.com>
2016-10-11 07:21:39 +0900

    1) Task #2335 (Add support for error_url): reverted, 2) Task #2664 (Change default timeout for HTTP requests from 5 to 10 seconds): done

--- src/plivo/rest/freeswitch/outboundsocket.py
+++ src/plivo/rest/freeswitch/outboundsocket.py
@@ -61,8 +61,7 @@ def assimilate_plivo_config(Obj, PlivoConfigStr):
     params = parse_params(PlivoConfigStr, ";", "=")
     if params.has_key('answer_url'):
         Obj.target_url = params['answer_url']
-    if params.has_key('error_url'):
-        Obj.error_url = params['error_url']
+        Obj.error_url = params['answer_url']
     else:
         Obj.error_url = None
     if params.has_key('flags'):
